c8f3c559c218aea24bd6fce58ef258e9049040fa
icromeroa <icromeroa@academia.usbbog.edu.co>
2025-09-20 17:22:34 +0000

    final

--- C/mis_ejercicios.c
+++ C/mis_ejercicios.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 void mi_ejemplo_1(){
     int num1 = 3;
     int *p = &num1;
     printf("Ejemplo 1: num = %d, puntero = %d\n", num1, *p);
 }
+
 
 void mi_ejemplo_2(){
     int num2 = 30;
@@ -15,12 +17,14 @@ void mi_ejemplo_2(){
     printf("Ejemplo 2: num = %d, la posición final = %d\n", num2, *p);
 }
 
+
 void mi_ejemplo_3(){
     int array[5] = {1, 4, 6, 7, 8};
     int *p = array;
     *p = array[4];
     printf("Ejemplo 3: la posición 4 del array = %d\n", *p);
 }
+
 
 void mi_ejemplo_4(){
     int array[7] = {2, 48, 78, 4, 5, 7, 7};
@@ -32,10 +36,12 @@ void mi_ejemplo_4(){
     printf("\n");
 }
 
+
 struct Persona {
     char nombre[20];
     int edad;
 };
+
 
 void mi_ejemplo_5() {
     struct Persona p2 = {"Juan", 25};
@@ -43,24 +49,31 @@ void mi_ejemplo_5() {
     printf("Ejemplo 5: %s tiene %d años\n", ptr2->nombre, ptr2->edad);
 };
 
+
 void mi_ejemplo_6() {
     int x = 10;
     int *p = &x;
     int **pp = &p;
 
+
     printf("Ejemplo 6: x = %d, *p = %d, **pp = %d\n", x, *p, **pp);
 }
+
+
 
 
 void incrementar(int *n) {
     (*n)++;
 }
 
+
 void mi_ejemplo_7() {
     int x = 25;
     incrementar(&x);
     printf("Ejemplo 7: x incrementado = %d\n", x);
 }
+
+
 
 
 void mi_ejemplo_8() {
@@ -73,6 +86,8 @@ void mi_ejemplo_8() {
 }
 
 
+
+
 void mi_ejemplo_9() {
     char *nombres[4] = {"Pedro", "Juan", "Carlos", "Lucia"};
     printf("Ejemplo 9: ");
@@ -82,22 +97,26 @@ void mi_ejemplo_9() {
     printf("\n");
 }
 
+
 int suma(int a, int b) { return a + b; }
 int resta(int a, int b) { return a - b; }
 int multiplicacion(int a, int b) { return a * b; }
 
+
 void mi_ejemplo_10() {
     int (*operacion)(int, int);
-    
+   
     operacion = suma;
     printf("Ejemplo 10: Suma = %d\n", operacion(2, 3));
-    
+   
     operacion = resta;
     printf("Ejemplo 10: Resta = %d\n", operacion(5, 2));
-    
+   
     operacion = multiplicacion;
     printf("Ejemplo 10: Multiplicacion = %d\n", operacion(4, 3));
 }
+
+
 
 
 int main() {
